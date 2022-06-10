b0420125ef8b1177e9c49e836b05fd1deccdb9e9
angelicapedroso <angelicapedroso05@gmail.com>
2022-06-10 10:11:41 -0300

    feat: query that displays username, number of songs played and total minutes

---
+++ desafio3.sql
@@ -0,0 +1,12 @@
+SELECT
+  u.name AS usuario,
+  COUNT(h.user_id) AS qtde_musicas_ouvidas,
+  ROUND(SUM(s.duration) / 60, 2) AS total_minutos
+FROM
+  SpotifyClone.user AS u
+  JOIN history AS h ON u.id = h.user_id
+  JOIN song AS s ON s.id = h.song_id
+GROUP BY
+  u.name
+ORDER BY
+  u.name;
